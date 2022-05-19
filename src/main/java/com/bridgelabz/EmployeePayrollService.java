e558d52a89bd1edf726bc430cf959411d359ff3d
SwatiBadole <swatibadole1997@gmail.com>
2022-05-19 14:42:40 -0700

    Added:Welcome Message

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class EmployeePayrollService {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to EmployeePayrollService");
+    }
+}
